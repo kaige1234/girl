b387b0102a6d79d26336c0973d5ffaf10e3234e6
286190665@qq.com <286190665@qq.com>
2018-09-23 17:02:12 +0800

    SunKaiService dev 合并了girl-feature-1-sunkai版本的   添加了信息

--- src/main/java/cn/com/ssm/Iservice/SunKaiService.java
+++ src/main/java/cn/com/ssm/Iservice/SunKaiService.java
@@ -2,8 +2,8 @@ package cn.com.ssm.Iservice;
 
 public class SunKaiService {
     public static void main(){
-        System.out.println("girl-dev----111");
-        System.out.println("girl-dev----222");
-        System.out.println("girl-dev----333");
+        System.out.println("girl-feature-1-sk-111");
+        System.out.println("girl-feature-1-sk-222");
+        System.out.println("girl-feature-1-sk-333");
     }
 }
